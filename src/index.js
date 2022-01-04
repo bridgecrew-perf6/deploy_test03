328522ae0e0f3c7468106200a474a31b99fa37b8
supermegageil <kimyeojin133@gmail.com>
2022-01-04 12:59:54 +0900

    Add hompage

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import {BrowserRouter} from 'react-router-dom';
+import {BrowserRouter as Router} from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <Router basename={process.env.PUBLIC_URL
+}>
     <App />
-    </BrowserRouter>
+    </Router>
     </React.StrictMode>,
     
   document.getElementById('root')
